e52039eb9fb55a5de2436405eee159012a19b2a5
Jakub Hirnšal <jakub.hirnsal@bisnode.com>
2021-09-26 12:47:24 +0200

    Refactor to parametrized test

--- src/test/java/com/sda/weatherlady/service/AverageCalculatorTest.java
+++ src/test/java/com/sda/weatherlady/service/AverageCalculatorTest.java
@@ -3,53 +3,131 @@ package com.sda.weatherlady.service;
 import com.sda.weatherlady.dto.CurrentDTO;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class AverageCalculatorTest {
 
-    @Test
-    public void testCalculateAverage() {
-        //given
-        List<CurrentDTO> data = new ArrayList<>();
-        data.add(AverageCalculator.buildAverage(
-                1020d,
-                25d,
-                300d,
-                10d,
-                "C",
-                "mb",
-                "km/h"
-        ));
-        data.add(AverageCalculator.buildAverage(
-                1040d,
-                27d,
-                320d,
-                14d,
-                "C",
-                "mb",
-                "km/h"
-        ));
-
+    @ParameterizedTest
+    @MethodSource("calculateAverageInputParams")
+    public void testCalculateAverage(CurrentDTO dto1, CurrentDTO dto2, CurrentDTO expected) {
         //when
-        CurrentDTO actual = AverageCalculator.calculateAverage(data.toArray(new CurrentDTO[0]));
+        CurrentDTO actual = AverageCalculator.calculateAverage(dto1, dto2);
 
         //then
         Assertions.assertEquals(
-                26f,
+                expected.getTemperature().getMetric().getValue(),
                 actual.getTemperature().getMetric().getValue()
         );
         Assertions.assertEquals(
-                1030f,
+                expected.getPressure().getMetric().getValue(),
                 actual.getPressure().getMetric().getValue()
         );
         Assertions.assertEquals(
-                310,
+                expected.getWind().getDirection().getDegrees(),
                 actual.getWind().getDirection().getDegrees()
         );
         Assertions.assertEquals(
-                12,
+                expected.getWind().getSpeed().getMetric().getValue(),
                 actual.getWind().getSpeed().getMetric().getValue()
         );
+    }
+
+    public static Stream<Arguments> calculateAverageInputParams() {
+        List<List<CurrentDTO>> data = new ArrayList<>();
+        data.add(List.of(
+                AverageCalculator.buildAverage(
+                        1020d,
+                        25d,
+                        300d,
+                        10d,
+                        "C",
+                        "mb",
+                        "km/h"
+                ),
+                AverageCalculator.buildAverage(
+                        1040d,
+                        27d,
+                        320d,
+                        14d,
+                        "C",
+                        "mb",
+                        "km/h"
+                ),
+                AverageCalculator.buildAverage(
+                        1030d,
+                        26d,
+                        310d,
+                        12d,
+                        "C",
+                        "mb",
+                        "km/h"
+                )
+        ));
+
+        data.add(List.of(
+                AverageCalculator.buildAverage(
+                        1000d,
+                        20d,
+                        250d,
+                        1d,
+                        "C",
+                        "mb",
+                        "km/h"
+                ),
+                AverageCalculator.buildAverage(
+                        1100d,
+                        30d,
+                        300d,
+                        3d,
+                        "C",
+                        "mb",
+                        "km/h"
+                ),
+                AverageCalculator.buildAverage(
+                        1050d,
+                        25d,
+                        275d,
+                        2d,
+                        "C",
+                        "mb",
+                        "km/h"
+                )
+        ));
+
+        data.add(List.of(
+                AverageCalculator.buildAverage(
+                        0d,
+                        5d,
+                        0d,
+                        0d,
+                        "C",
+                        "mb",
+                        "km/h"
+                ),
+                AverageCalculator.buildAverage(
+                        -2d,
+                        -5d,
+                        0d,
+                        14d,
+                        "C",
+                        "mb",
+                        "km/h"
+                ),
+                AverageCalculator.buildAverage(
+                        -1d,
+                        0d,
+                        0d,
+                        7d,
+                        "C",
+                        "mb",
+                        "km/h"
+                )
+        ));
+
+        return data.stream().map(list -> Arguments.of(list.toArray()));
     }
 }
