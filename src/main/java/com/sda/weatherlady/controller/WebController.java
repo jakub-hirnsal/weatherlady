03d84b1159e3cfb59a6f3144712ddc54c04d021f
Jakub Hirnšal <jakub.hirnsal@bisnode.com>
2021-10-02 13:49:13 +0200

    Implement Web inteface  - Add thymeleaf, bootstrap to pom  - Add WebController  - Add views, css, js (assets)

---
+++ src/main/java/com/sda/weatherlady/controller/WebController.java
@@ -0,0 +1,52 @@
+package com.sda.weatherlady.controller;
+
+import com.sda.weatherlady.model.CurrentCondition;
+import com.sda.weatherlady.repository.CurrentConditionRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller // goes into WebApplicationContext
+@RequestMapping(value = "/web")
+public class WebController {
+
+    private final CurrentConditionRepository currentConditionRepository;
+
+    public WebController(CurrentConditionRepository currentConditionRepository) {
+        this.currentConditionRepository = currentConditionRepository;
+    }
+
+    @GetMapping(
+            value = "/index"
+    )
+    public String getIndex(
+            final ModelMap modelMap,
+            @RequestParam(defaultValue = "unknown") String name
+    ) {
+        modelMap.addAttribute("name", name);
+
+
+        return "index";
+    }
+
+    @GetMapping(
+            value = "/conditions"
+    )
+    public String showAllConditions(final ModelMap modelMap) {
+        Iterable<CurrentCondition> conditionIterable = currentConditionRepository.findAll();
+
+        List<CurrentCondition> currentConditionList = StreamSupport.stream(conditionIterable.spliterator(), false)
+                .collect(Collectors.toList());
+
+        modelMap.addAttribute("listOfConditions", currentConditionList);
+
+        return "conditions";
+    }
+
+
+}
